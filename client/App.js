c8ca6dcc9503558be03f858b600bc8be3b640532
vimukthinirmana <vimukthinirmana64@gmail.com>
2024-08-02 21:15:20 +0530

    update content

--- client/App.js
+++ client/App.js
@@ -1,21 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './navigation';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text className='text-red-500 text-4xl'>Hi Kvns..</Text>
-      
-      <StatusBar style="auto" />
-    </View>
+    <Navigation />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ client/screens/StoreScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function StoreScreen() {
+  return (
+    <View>
+      <Text>StoreScreen</Text>
+    </View>
+  )
+}
